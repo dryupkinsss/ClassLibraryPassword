033e5900966609c727147b0164fe647d8bf8e554
student <student@PC-VOL-53-23>
2023-03-09 12:55:59 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryPasswordTests/PasswordCheckerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryPassword;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPassword.Tests
+{
+    [TestClass()]
+    public class PasswordCheckerTests
+    {
+        [TestMethod()]
+        public void Check_8Symbols_ReturnsTrue()
+        {
+            // Arrange.
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Check_4Symbols_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "Aq1$";
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void Check_PasswordWidthLowerSymbols_ReturnsTrue()
+        {
+            // Arrange.
+            string password = "ASDq123$";
+            bool expected = true;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Check_PasswordWidthLowerSymbols_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "ASDQWE123$";
+            bool expected = false;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
